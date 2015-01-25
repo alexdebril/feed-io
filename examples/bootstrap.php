cf952eebd19ffa629d412bac85e68ce7eaa466e4
Alex Debril <alex.debril@gmail.com>
2015-01-25 12:32:58 +0100

    Example code for parsing RSS stream

---
+++ examples/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of the feed-io package.
+ *
+ * (c) Alexandre Debril <alex.debril@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$loader = require __DIR__ . "/../vendor/autoload.php";
+
+date_default_timezone_set('UTC');
