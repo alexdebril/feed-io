dc95b63d3ec2e2dfd5ed972a46279b4f59848f3a
Alex Debril <alex.debril@gmail.com>
2015-01-25 15:58:41 +0100

    Guzzle Client implementation

---
+++ examples/guzzle-client.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the feed-io package.
+ *
+ * (c) Alexandre Debril <alex.debril@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+$client = new \FeedIo\Adapter\Guzzle\Client(new GuzzleHttp\Client());
+
+$response = $client->getResponse('http://php.net/feed.atom', new \DateTime());
+
+echo $response->getBody();
