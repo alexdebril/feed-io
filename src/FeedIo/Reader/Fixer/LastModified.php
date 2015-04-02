a64c1b676dafee01232f05cb2b2319943efadc9e
Alex Debril <alex.debril@gmail.com>
2015-04-02 09:22:40 +0200

    Feed fixer system

---
+++ src/FeedIo/Reader/Fixer/LastModified.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * This file is part of the feed-io package.
+ *
+ * (c) Alexandre Debril <alex.debril@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FeedIo\Reader\Fixer;
+
+use FeedIo\FeedInterface;
+use FeedIo\Reader\FixerInterface;
+
+class LastModified implements FixerInterface
+{
+
+    public function setRight(FeedInterface $feed)
+    {
+        if ( is_null($feed->getLastModified()) ) {
+            $feed->setLastModified(
+                        $this->searchLastModified($feed)
+            );
+        }
+        
+        return $this;
+    }
+
+    public function searchLastModified(FeedInterface $feed)
+    {
+        $latest = new \DateTime('@0');
+        
+        foreach ( $feed as $item ) {
+            if ( $item->getLastModified() > $latest ) {
+                $latest = $item->getLastModified();
+            }
+        }
+        
+        return $latest;
+    }
+
+}
