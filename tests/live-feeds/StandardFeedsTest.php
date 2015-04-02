22309c8adddc0912a48ee97e6bd4a0b781dfdec0
Alex Debril <alex.debril@gmail.com>
2015-04-02 23:25:10 +0200

    Live feeds URLs

--- tests/live-feeds/StandardFeedsTest.php
+++ tests/live-feeds/StandardFeedsTest.php
@@ -46,7 +46,6 @@ class StandardFeedsTest extends \PHPUnit_Framework_TestCase
         $this->performStringAssertions(
             array(
                 'title' => $feed->getTitle(),
-                'id' => $feed->getPublicId(),
                 'link' => $feed->getLink(),
             )
         );
@@ -60,10 +59,10 @@ class StandardFeedsTest extends \PHPUnit_Framework_TestCase
     
     protected function performItemAssertions(\FeedIo\Feed\ItemInterface $item) 
     {
+        $this->assertInstanceOf('\DateTime', $item->getLastModified());
         $this->performStringAssertions(
             array(
                 'title' => $item->getTitle(),
-                'id' => $item->getPublicId(),
                 'link' => $item->getLink(),
                 'description' => $item->getDescription(),
             )
@@ -110,7 +109,39 @@ class StandardFeedsTest extends \PHPUnit_Framework_TestCase
             'http://127.0.0.1:8080/feed-io/tests/samples/rss/sample-rss.xml',
         );
         
-        $urls = array();
+        $urls = array(
+            'http://feeds.bbci.co.uk/news/rss.xml?edition=uk',
+            'http://feeds.feedburner.com/dailyjs',
+            'http://feeds.feedburner.com/HighScalability',
+            'http://feeds.feedburner.com/symfony/blog',
+            'http://feeds.mashable.com/Mashable',
+            'http://feeds.slate.com/slate',
+            'http://feeds.wired.com/wired/index',
+            'http://feeds2.feedburner.com/blogspot/Egta',
+            'http://feeds2.feedburner.com/LeJournalduGeek',
+            'http://feeds2.feedburner.com/Webappers',
+            'http://liberation.fr.feedsportal.com/c/32268/fe.ed/rss.liberation.fr/rss/44/',
+            'http://liberation.fr.feedsportal.com/c/32268/fe.ed/rss.liberation.fr/rss/54/',
+            'http://liberation.fr.feedsportal.com/c/32268/fe.ed/rss.liberation.fr/rss/latest/',
+            'http://linuxfr.org/journaux.atom',
+            'http://php.net/feed.atom',
+            'http://pipes.yahoo.com/pipes/pipe.run?_id=647030be6aceb6d005c3775a1c19401c&_render=rss',
+            'http://rss.lemonde.fr/c/205/f/3050/index.rss',
+            'http://rss.slashdot.org/Slashdot/slashdot',
+            'http://rue89.feedsportal.com/c/33822/f/608948/index.rss',
+            'http://what-if.xkcd.com/feed.atom',
+            'http://www.debian.org/News/news',
+            'http://www.gizmodo.fr/feed',
+            'http://www.larvf.com/rss/article/10135',
+            'http://www.lemonde.fr/sciences/rss_full.xml',
+            'http://www.lemonde.fr/technologies/rss_full.xml',
+            'http://www.lepoint.fr/vin/rss.xml',
+            'http://www.metalorgie.com/feed/news',
+            'http://www.sitepoint.com/feed/',
+            'http://www.slate.fr/rss.xml',
+            'http://xkcd.com/rss.xml',
+            'https://fr.news.yahoo.com/?format=rss',
+        );
         
         return $this->isLocalhostUp() ? array_merge($localhost, $urls):$urls;
     }
