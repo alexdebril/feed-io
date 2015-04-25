17ea10b75eb852ab8f49ddfb9d2346aea4d5f47a
Alex Debril <alex.debril@gmail.com>
2015-04-25 13:09:27 +0200

    Enclosure support tests

--- tests/FeedIo/Parser/RssTest.php
+++ tests/FeedIo/Parser/RssTest.php
@@ -19,12 +19,33 @@ class RssTest extends ParserTestAbstract
 {
 
     const SAMPLE_FILE = 'rss/sample-rss.xml';
-
+    
+    const ENCLOSURE_FILE = 'rss/rss-enclosure.xml';
+    
     /**
      * @return \FeedIo\StandardAbstract
      */
     public function getStandard()
     {
         return new Rss(new DateTimeBuilder());
+    }
+    
+    public function testEnclosure()
+    {
+        $document = $this->buildDomDocument(static::ENCLOSURE_FILE);
+        $feed = $this->object->parse($document, new Feed());
+        
+        $count = 0;
+        foreach ( $feed as $item ) {
+            $count++;
+            $this->assertTrue($item->hasMedia());
+            $media = $item->getMedias()->current();
+            
+            $this->assertInstanceOf('\FeedIo\Feed\Item\MediaInterface', $media);
+            $this->assertEquals('audio/mpeg', $media->getType());
+            $this->assertInternalType('string', $media->getUrl());
+        }
+        
+        $this->assertEquals(1, $count);
     }
 }
