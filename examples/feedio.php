5c7d6b28353f46a5b70b7804c7ce48638126ab36
Alex Debril <alex.debril@gmail.com>
2015-04-29 20:39:32 +0200

    media support

--- examples/feedio.php
+++ examples/feedio.php
@@ -20,6 +20,15 @@ echo "feed title : {$result->getFeed()->getTitle()} \n " ;
 
 foreach( $result->getFeed() as $item ) {
     echo "item title : {$item->getTitle()} \n ";
+
+    // let's turn php.net into a PodCast
+    $media = new \FeedIo\Feed\Item\Media
+    $media->setUrl('http://yourdomain.tld/medias/some-podcast.mp3');
+    $media->setType('audio/mpeg');
+ 
+    // add it to the item
+    $item->addMedia($media);
 }
 
 $domDocument = $feedIo->toAtom($result->getFeed());
+echo $domDocument->saveXML();
