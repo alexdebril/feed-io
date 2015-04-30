f8f94c1b188abe105d1e95ad3784ff5d39f41d5c
Alex Debril <alex.debril@gmail.com>
2015-04-30 21:42:51 +0200

    media example

---
+++ examples/media.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of the feed-io package.
+ *
+ * (c) Alexandre Debril <alex.debril@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+$feed = new \FeedIo\Feed;
+
+// ask for a new Item
+$item = $feed->newItem();
+
+// build the media
+$media = new \FeedIo\Feed\Item\Media
+$media->setUrl('http://yourdomain.tld/medias/some-podcast.mp3');
+$media->setType('audio/mpeg');
+ 
+// add it to the item
+$item->addMedia($media);
+
+$client = new \FeedIo\Adapter\NullClient();
+
+$logger = new \Psr\Log\NullLogger();
+
+$feedIo = new \FeedIo\FeedIo($client, $logger);
+$domDocument = $feedIo->toAtom($result->getFeed());
+echo $domDocument->saveXML();
