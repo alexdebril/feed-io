955048f611b67a02210eb986ed5e83d9aabc5a45
Alex Debril <alex.debril@gmail.com>
2015-08-26 20:31:38 +0200

    category rule

---
+++ src/FeedIo/Rule/Category.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * This file is part of the feed-io package.
+ *
+ * (c) Alexandre Debril <alex.debril@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FeedIo\Rule;
+
+use FeedIo\Feed\Node\CategoryInterface;
+use FeedIo\Feed\NodeInterface;
+use FeedIo\RuleAbstract;
+
+class Category extends RuleAbstract
+{
+
+    const NODE_NAME = 'category';
+        
+    /**
+     * @param  NodeInterface $node
+     * @param  \DOMElement   $element
+     * @return mixed
+     */
+    public function setProperty(NodeInterface $node, \DOMElement $element)
+    {
+        $category = $node->newCategory();
+        $category->setScheme($this->getAttributeValue($element, 'domain'))
+        ->setLabel($element->nodeValue)
+        ->setTerm($element->nodeValue);
+        $node->addCategory($category);        
+
+        return $this;
+    }
+
+    /**
+     * creates the accurate DomElement content according to the $item's property
+     *
+     * @param  \DomDocument  $document
+     * @param  NodeInterface $node
+     * @return \DomElement
+     */
+    public function createElement(\DomDocument $document, NodeInterface $node)
+    {
+        foreach( $node->getCategories() as $category ) {
+            return $this->createCategoryElement($document, $category);
+        }
+    }
+    
+    /**
+     * @param  \DomDocument   $document
+     * @param  CategoryInterface $category
+     * @return \DomElement
+     */
+    public function createCategoryElement(\DomDocument $document, CategoryInterface $category)
+    {
+        $element = $document->createElement(
+            $this->getNodeName(),
+            is_null($category->getTerm()) ? $category->getLabel():$category->getTerm()
+            );
+        $element->setAttribute('domain', $category->getScheme());
+
+        return $element;
+    }
+}
---
+++ tests/FeedIo/Rule/CategoryTest.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * This file is part of the feed-io package.
+ *
+ * (c) Alexandre Debril <alex.debril@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FeedIo\Rule;
+
+use FeedIo\Feed\Item;
+
+class CategoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \FeedIo\Rule\Category
+     */
+    protected $object;
+
+    protected function setUp()
+    {
+        $this->object = new Category();
+    }
+    
+    public function testSetProperty()
+    {
+        $item = new Item();
+        
+        $element = new \DomElement('category', 'foo');
+        $this->object->setProperty($item, $element);
+                
+        $count = 0;
+        foreach ($item->getCategories() as $category) {
+            $count++;
+            $this->assertEquals('foo', $category->getTerm());                    
+            $this->assertEquals('foo', $category->getLabel());
+        }
+        
+        $this->assertEquals(1, $count);
+    }
+    
+    public function testCreateCategoryElement()
+    {
+        $category = new \FeedIo\Feed\Node\Category();
+        $category->setLabel('foo');
+        $category->setScheme('bar');
+        
+        $element = $this->object->createCategoryElement(new \DomDocument(), $category);
+        
+        $this->assertEquals('foo', $element->nodeValue);
+        $this->assertEquals('bar', $element->getAttribute('domain'));
+    }
+    
+    public function testCreateCategoryElementUsingTerm()
+    {
+        $category = new \FeedIo\Feed\Node\Category();
+        $category->setTerm('foo');
+        
+        $element = $this->object->createCategoryElement(new \DomDocument(), $category);
+        
+        $this->assertEquals('foo', $element->nodeValue);
+    }
+    
+    public function testCreateElement()
+    {
+        $category = new \FeedIo\Feed\Node\Category();
+        $category->setLabel('foo');
+        
+        $item = new Item;
+        $item->addCategory($category);
+        
+        $element = $this->object->createElement(new \DomDocument, $item);
+        
+        $this->assertEquals('foo', $element->nodeValue);
+    }
+    
+}
