b9a83ca40b68e52dff99f018a5d42660c10b4649
Alex Debril <alex.debril@gmail.com>
2016-01-15 09:35:51 +0100

    Builder interfaces

---
+++ src/FeedIo/Factory/ClientBuilderInterface.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of the feed-io package.
+ *
+ * (c) Alexandre Debril <alex.debril@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FeedIo\Factory;
+
+/**
+ * @package FeedIo
+ */
+interface ClientBuilderInterface extends BuilderInterface
+{
+
+    /**
+     * This method MUST return a \FeedIo\Adapter\ClientInterface instance
+     * @return \FeedIo\Adapter\ClientInterface
+     */
+    public function getClient();
+
+}
