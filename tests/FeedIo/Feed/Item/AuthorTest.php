dd0705c2b0f10f0bbfc0044f7cb5d7e45ee5c1f9
Alex Debril <alex.debril@gmail.com>
2017-05-02 14:39:02 +0200

    Author Entity (issue #72)

---
+++ tests/FeedIo/Feed/Item/AuthorTest.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * This file is part of the feed-io package.
+ *
+ * (c) Alexandre Debril <alex.debril@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FeedIo\Feed\Item;
+
+class AuthorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \FeedIo\Feed\Item\Author
+     */
+    protected $object;
+
+    protected function setUp()
+    {
+        $this->object = new Author();
+    }
+
+    public function testSetName()
+    {
+        $this->object->setName('John Doe');
+        $this->assertEquals('John Doe', $this->object->getName());
+    }
+
+    public function testSetUri()
+    {
+        $this->object->setUri('http://localhost');
+        $this->assertEquals('http://localhost', $this->object->getUri());
+    }
+
+    public function testSetEmail()
+    {
+        $this->object->setEmail('john@localhost');
+        $this->assertEquals('john@localhost', $this->object->getEmail());
+    }
+}
