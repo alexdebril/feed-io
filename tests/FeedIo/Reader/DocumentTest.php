542eb1cabfe91bdd29c852293a47144ba2e7d9e0
Alex Debril <alex.debril@gmail.com>
2017-06-14 17:06:34 +0200

    Document object

---
+++ tests/FeedIo/Reader/DocumentTest.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * This file is part of the feed-io package.
+ *
+ * (c) Alexandre Debril <alex.debril@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FeedIo\Reader;
+
+use FeedIo\Feed;
+
+class DocumentTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testIsJson()
+    {
+        $document = new Document('{"json": "value"}');
+
+        $this->assertTrue($document->isJson());
+    }
+
+    public function testIsXml()
+    {
+        $document = new Document('<feed xmlns="http://www.w3.org/2005/Atom"></feed>');
+
+        $this->assertTrue($document->isXml());
+    }
+
+
+}
