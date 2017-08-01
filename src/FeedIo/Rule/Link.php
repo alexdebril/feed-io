85a4e705045750e852c12b4c6c9b78d254bfe7fe
Alex Debril <alex.debril@gmail.com>
2017-08-01 13:54:17 +0200

    Link rule adaptation

--- src/FeedIo/Rule/Link.php
+++ src/FeedIo/Rule/Link.php
@@ -30,14 +30,18 @@ class Link extends RuleAbstract
     }
 
     /**
-     * creates the accurate DomElement content according to the $item's property
-     *
-     * @param  \DomDocument  $document
-     * @param  NodeInterface $node
-     * @return \DomElement
+     * @inheritDoc
      */
-    public function createElement(\DomDocument $document, NodeInterface $node)
+    protected function hasValue(NodeInterface $node) : bool
     {
-        return $document->createElement($this->getNodeName(), $node->getLink());
+        return !! $node->getLink();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function addElement(\DomDocument $document, \DOMElement $rootElement, NodeInterface $node) : void
+    {
+        $rootElement->appendChild($document->createElement($this->getNodeName(), $node->getLink()));
     }
 }
