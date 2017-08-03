bec9de70109ec0f430d402277651d27f269884a6
Alex Debril <alex.debril@gmail.com>
2017-08-03 09:18:25 +0200

    Factory return types

--- tests/FeedIo/FactoryTest.php
+++ tests/FeedIo/FactoryTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace FeedIo;
 
+use FeedIo\Factory\BuilderInterface;
 use \PHPUnit\Framework\TestCase;
 
 class FactoryTest extends TestCase
@@ -148,9 +149,25 @@ class FactoryTest extends TestCase
     }
 }
 
-class ExternalBuilder
+class ExternalBuilder implements BuilderInterface
 {
     public function __construct(array $config)
     {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getMainClassName()
+    {
+        return 'main';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPackageName()
+    {
+        return 'package';
     }
 }
