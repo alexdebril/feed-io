c22ab80b6ca9c9af08373f3e6343ded763edcf74
Alex Debril <alex.debril@gmail.com>
2018-05-29 08:33:24 +0200

    Inject a custom user-agent through __construct

--- examples/change-user-agent.php
+++ examples/change-user-agent.php
@@ -19,7 +19,8 @@ $stack->push(
 $client = new \FeedIo\Adapter\Guzzle\Client(
     new GuzzleHttp\Client([
         'handler' => $stack
-        ])
+        ]),
+    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
 );
 
 $feedIo = new \FeedIo\FeedIo($client, $logger);
