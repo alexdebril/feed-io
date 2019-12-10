f1242e70212aa6ea373f9fb2b67d49fbee6a6272
Éloi Rivard <azmeuk@gmail.com>
2019-12-10 18:05:23 +0100

    Deprecated MediaInterface::isThumbnail

--- src/FeedIo/Feed/Item/Media.php
+++ src/FeedIo/Feed/Item/Media.php
@@ -68,10 +68,12 @@ class Media implements MediaInterface
     }
 
     /**
+     * @deprecated
      * @return bool
      */
     public function isThumbnail() : bool
     {
+        trigger_error('Method isThumbnail is deprecated and will be removed in feed-io 5.0', E_USER_DEPRECATED);
         return $this->nodeName === 'media:thumbnail';
     }
 
