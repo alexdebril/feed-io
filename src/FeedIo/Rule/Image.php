cb84e0aab562950a282bac996b6010aabc383416
alexdebril <alexdebril@users.noreply.github.com>
2021-01-19 22:11:52 +0100

    support unspecified 'image' tag (#317)
    * support unspecified 'image' tag
    
    * Unit test for Image rule

---
+++ src/FeedIo/Rule/Image.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+
+namespace FeedIo\Rule;
+
+use FeedIo\Feed\ItemInterface;
+use FeedIo\Feed\NodeInterface;
+use FeedIo\RuleAbstract;
+
+class Image extends RuleAbstract
+{
+    const NODE_NAME = 'image';
+
+    public function setProperty(NodeInterface $node, \DOMElement $element): void
+    {
+        if ($node instanceof ItemInterface) {
+            $media = new \FeedIo\Feed\Item\Media();
+            $media->setUrl($element->textContent);
+            $node->addMedia($media);
+        }
+    }
+
+    protected function hasValue(NodeInterface $node): bool
+    {
+        return false;
+    }
+
+    protected function addElement(\DomDocument $document, \DOMElement $rootElement, NodeInterface $node): void
+    {
+        throw new \RuntimeException("you should not try to write a <image> tag");
+    }
+}
---
+++ tests/FeedIo/Rule/ImageTest.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace FeedIo\Rule;
+
+
+use FeedIo\Feed\Item;
+use PHPUnit\Framework\TestCase;
+
+class ImageTest extends TestCase
+{
+
+    protected $object;
+
+    protected function setUp(): void
+    {
+        $this->object = new Image();
+    }
+
+    public function testSetProperty(): void
+    {
+        $document = new \DomDocument();
+        $media = $document->createElement('image');
+        $media->textContent = 'http://localhost';
+
+        $item = new Item();
+        $this->object->setProperty($item, $media);
+        $this->assertTrue($item->hasMedia());
+
+        $count = 0;
+        foreach ($item->getMedias() as $itemMedia) {
+            $this->assertIsString($itemMedia->getUrl());
+
+            $this->assertEquals($media->textContent, $itemMedia->getUrl());
+            $count++;
+        }
+
+        $this->assertEquals(1, $count);
+    }
+
+}
