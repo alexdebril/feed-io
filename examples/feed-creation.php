0f2a2c83eaed4bb41cc251e8551539ff17c9e8aa
Alex Debril <alexdebril@users.noreply.github.com>
2021-03-09 22:28:09 +0100

    Updated with summary + content

--- examples/feed-creation.php
+++ examples/feed-creation.php
@@ -16,6 +16,7 @@ use \FeedIo\Feed;
 $feed = new Feed();
 $feed->setLink('https://feed-io.net');
 $feed->setTitle('feed-io example feed');
+$feed->setDescription('my greate feed');
 
 // The item instance SHOULD be instanciated by the feed
 $item = $feed->newItem();
@@ -23,8 +24,8 @@ $item = $feed->newItem();
 $item->setTitle('a title');
 $item->setLastModified(new \DateTime());
 $item->setLink('https://feed-io.net/item/1');
-$item->setDescription("Hope you like the code you are reading");
-
+$item->setContent("Hope you like the code you are reading");
+$item->setSummary('my summary');
 $feed->add($item);
 
 $feedIo = Factory::create()->getFeedIo();
