e239de25abacb23d3991526fc151225cabbc0c97
Accalia Elementia <accalia@elementia.me>
2022-09-21 06:01:50 -0400

    Improve handling of feeds when discover encounters feel links without a protocol (#405)
    Signed-off-by: Accalia <Accalia@Elementia.me>
    
    Signed-off-by: Accalia <Accalia@Elementia.me>
    Co-authored-by: alexdebril <alexdebril@users.noreply.github.com>

--- tests/FeedIo/ExplorerTest.php
+++ tests/FeedIo/ExplorerTest.php
@@ -28,8 +28,13 @@ class ExplorerTest extends TestCase
 
     public function setUp(): void
     {
+        
+    }
+
+    protected function createTestObject(string $htmlPath)
+    {
         $this->object = new Explorer(
-            $this->getClientMock(),
+            $this->getClientMock($htmlPath),
             new NullLogger()
         );
     }
@@ -37,9 +42,9 @@ class ExplorerTest extends TestCase
     /**
      * @return \FeedIo\Adapter\ClientInterface
      */
-    protected function getClientMock()
+    protected function getClientMock(string $htmlPath)
     {
-        $html = file_get_contents(dirname(__FILE__)."/../samples/discovery.html");
+        $html = file_get_contents(dirname(__FILE__) . $htmlPath);
         $client = $this->createMock('FeedIo\Adapter\ClientInterface');
         $response = $this->createMock('FeedIo\Adapter\ResponseInterface');
         $response->expects($this->any())->method('getBody')->will($this->returnValue($html));
@@ -53,8 +58,31 @@ class ExplorerTest extends TestCase
      */
     public function testDiscover()
     {
+        $this->createTestObject("/../samples/discovery.html");
         $feeds = $this->object->discover('https://exmple.org/feed.atom');
 
         $this->assertEquals(['http://example.org/feed.xml', 'http://example.org/comments.xml'], $feeds);
+    }
+
+    /**
+     * @covers \FeedIo\Reader::addParser
+     */
+    public function testDiscoverHttps()
+    {
+        $this->createTestObject("/../samples/discovery-https.html");
+        $feeds = $this->object->discover('https://exmple.org/feed.atom');
+
+        $this->assertEquals(['https://example.org/feed.xml', 'https://example.org/comments.xml'], $feeds);
+    }
+
+    /**
+     * @covers \FeedIo\Reader::addParser
+     */
+    public function testDiscoverProtocolless()
+    {
+        $this->createTestObject("/../samples/discovery-mixed.html");
+        $feeds = $this->object->discover('https://exmple.org/feed.atom');
+
+        $this->assertEquals(['https://example.org/feed.xml', 'https://example.org/comments.xml'], $feeds);
     }
 }
